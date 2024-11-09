86c7ef4d63a3bbb7e898902fa27ccaddfda848d8
shikhar797 <shikharrawat2004@gmail.com>
2024-11-10 00:03:37 +0530

    .

--- data.cjs
+++ data.cjs
@@ -3,28 +3,79 @@ ITEMS=[
         name:"Caramel Tofee",
         price:575,
         quantity:1,
-        src:"/assets (1)/asset 6.png"
+        src:"/assets (1)/asset 6.png",
+        para:"Take a trip down memory lane with our old-fashioned Caramel Toffees ~  our tribute to traditional confectionery at its finest. Rich, buttery and golden, these caramel delights simply melt in your mouth. "
     },
     {
         name:"Strawberry Jam",
         price:475,
         quantity:1,
-        src:"/assets (1)/asset 7.png"
-        
+        src:"/assets (1)/asset 7.png",
+        para:"Like biting into a fresh strawberry. Our Strawberry Jam is a jarful of love, like warm hugs and cherished moments spent gathered around the kitchen table."
 
     },
     {
         name:"Three Citrus Marmalade",
         price:350,
         quantity:1,
-        src:"/assets (1)/asset 8.png"
+        src:"/assets (1)/asset 8.png",
+        para:"Take a trip down memory lane with our Three - Citrus Marmalade. Hearty, bittersweet, and true to tradition. "
 
     },
     {
         name:"Lemon Curd",
         price:400,
         quantity:1,
-        src:"/assets (1)/asset 9.png"
+        src:"/assets (1)/asset 9.png",
+        para:"A rich, lemony, golden custard made with the finest ingredients. Ideal to fill in tarts or, if you're lazy (like us), enjoy spoonfuls straight from the jar 😉"
+
+    },
+    {
+        name:"Plum Jam",
+        price:800,
+        quantity:1,
+        src:"/assets (1)/asset 10.png",
+        para:"Indulge in the flavour of fresh plums with our irresistible Plum Jam - a hit not only with adults but with little ones too! But don't just take our word for it - you've gotta taste it 😄"
+
+    },
+    {
+        name:"Tomato Pickle",
+        price:800,
+        quantity:1,
+        src:"/assets (1)/asset 11.png",
+        para:"Farm fresh tangy tomatoes combined with the crisp, sharp flavour of mustard - in true Himachali style!"
+
+    },
+    {
+        name:"Salted Caramel",
+        price:600,
+        quantity:1,
+        src:"/assets (1)/asset 12.png",
+        para:"Nutty, golden, slow-cooked caramel with flaky bursts of fresh Himalayan salt. Drizzle on desserts for simple indulgence. "
+
+    },
+    {
+        name:"Pears",
+        price:500,
+        quantity:1,
+        src:"/assets (1)/asset 13.png",
+        para:"Handpicked and carefully preserved pear halves in a light syrup with their natural sweetness. Enjoy them year round as a versatile ingredient in your favourite recipes.  "
+
+    },
+    {
+        name:"Pickle Vegitable",
+        price:755,
+        quantity:1,
+        src:"/assets (1)/asset 14.png",
+        para:"Featuring a rotating selection of fresh seasonal vegetables pickled in a tangy vinegar brine that makes for the best snack or compliment to your meals. "
+
+    },
+    {
+        name:"Gajar Gobhi Salgam",
+        price:450,
+        quantity:1,
+        src:"/assets (1)/asset 15.png",
+        para:"Crunchy garden-fresh vegetables pickled in a lip-smacking mix of traditional spices - no meal goes without it at Big Bear Farms!"
 
     },
 
